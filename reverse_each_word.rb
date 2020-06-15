857573261a5428f7241569d4bf514051795eba76
Hailey Ringier <haileyringier@outlook.com>
2020-06-15 15:53:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,9 @@
 def reverse_each_word (string)
  new_array = string.split (/ /)
+ reversed_array = []
   new_array.each do |element|
-    element.reverse
-end
-return reversed_string.join(" ")
+    reversed_element = element.reverse()
+    reversed_array << reversed_element
+  end
+return reversed_array.join(" ")
 end
